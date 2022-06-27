670a124773f077de7856f178cad410029bec4c95
Lee, Jaehwan <jaehwanee0411@gmail.com>
2022-06-28 00:31:05 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,3 +1,3 @@
-i=list(map(int,input().split()))
-print(max(i)*100) if len(set(i))==3 else print(10000+1000*i[0]) if len(set(i))==1 else \
-(print(1000+100*list(set(i))[0]) if i.count(list(set(i))[0])==2 else print(1000+100*list(set(i))[1]))
+n=int(input())
+for i in range(3):
+    print('*'*3)
