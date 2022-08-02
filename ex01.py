e3aa43285b219ffd0f118762c9d7e56113d6faa8
Lee, Jaehwan <jaehwanee0411@gmail.com>
2022-08-02 15:50:08 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,10 +1,14 @@
-
-def check(N):
-    for i in range(N):
-        if (i+sum(map(int,str(i)))) == N:
-            return i
-    return 0
-
-N = int(input())
-print(check(N))
-
+N=int(input())
+ls=[]
+idx=[]
+for i in range(N):
+    w,h=map(int,input().split())
+    ls.append((w,h))
+for i in ls:
+    cnt=0
+    for j in ls:
+        if i[0]<j[0] and i[1]<j[1]:
+            cnt+=1
+    idx.append(cnt+1)
+for i in idx:
+    print(i, end=' ')
