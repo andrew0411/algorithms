cd13f96fcc0468eb190685d9287c2d993a156f26
Lee, Jaehwan <jaehwanee0411@gmail.com>
2022-08-02 15:50:42 +0900

    Create 7568.py

---
+++ BOJ_7xxx/7568.py
@@ -0,0 +1,14 @@
+N=int(input())
+ls=[]
+idx=[]
+for i in range(N):
+    w,h=map(int,input().split())
+    ls.append((w,h))
+for i in ls:
+    cnt=0
+    for j in ls:
+        if i[0]<j[0] and i[1]<j[1]:
+            cnt+=1
+    idx.append(cnt+1)
+for i in idx:
+    print(i, end=' ')
